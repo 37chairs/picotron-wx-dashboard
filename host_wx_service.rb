72ed3d95bcfe6b7fe8ee0e23eea6e260729eaace
Bryan Thompson <bryan@37chairs.com>
2024-05-19 09:09:41 -0500

    notes

--- host_wx_service.rb
+++ host_wx_service.rb
@@ -1,5 +1,4 @@
 require 'json'
-
 module RubyPicoBridge
   class WX
     def initialize
@@ -7,7 +6,7 @@ module RubyPicoBridge
     end
 
     def get_wx_data
-      # todo: fetch from a weather API or from local home assistant
+      # TODO: fetch from a weather API or from local home assistant
       @wx_data = {
         time_hour: Time.now.strftime('%H').to_i,
         time_minute: Time.now.strftime('%M').to_i,
@@ -24,18 +23,20 @@ module RubyPicoBridge
 
     def format_wx_data
       payload = []
-      payload << format('%02x', @wx_data[:time_hour] & 0xFF) # HH
-      payload << format('%02x', @wx_data[:time_minute] & 0xFF) # MM
-      payload << format('%02x', @wx_data[:time_second] & 0xFF) # SS
-      payload << format('%02x', @wx_data[:temperature] & 0xFF)  # Ensure it's two characters
-      payload << format('%02x', @wx_data[:humidity] & 0xFF)     # Ensure it's two characters
-      payload << format('%04x', @wx_data[:pressure] & 0xFFFF)   # Ensure it's four characters for pressure
-      payload << format('%02x', @wx_data[:wind_speed] & 0xFF)   # Ensure it's two characters
+      payload << format('%02x', @wx_data[:time_hour] & 0xFF)
+      payload << format('%02x', @wx_data[:time_minute] & 0xFF)
+      payload << format('%02x', @wx_data[:time_second] & 0xFF)
+      payload << format('%02x', @wx_data[:temperature] & 0xFF)
+      payload << format('%02x', @wx_data[:humidity] & 0xFF)
+      payload << format('%04x', @wx_data[:pressure] & 0xFFFF)
+      payload << format('%02x', @wx_data[:wind_speed] & 0xFF)
       '0x' + payload.join('')
     end
 
     def store_wx_data
       file_path = File.expand_path('~/Library/Application Support/Picotron/drive/appdata/wx.dat')
+
+      # for now, we'll also write to json to sanity check our data format & for debugging.
       txt_file_path = File.expand_path('~/Library/Application Support/Picotron/drive/appdata/wx.txt')
       File.open(file_path, 'w') do |data_file|
         data_file.write(format_wx_data)
