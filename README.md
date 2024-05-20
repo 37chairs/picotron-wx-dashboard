bcdb26e6ed417d47f26cd897523763fc699c729e
Bryan Thompson <bryan@37chairs.com>
2024-05-19 22:39:09 -0500

    weather data & initial view

--- README.md
+++ README.md
@@ -2,23 +2,12 @@
 
 This is a simple wx dashboard for the Picotron.  It reads wx data from a shared location on the host computer and displays it on the Picotron screen.  The wx data is fetched from an API and written to the shared location by a separate script running on the host computer.
 
-![Picotron wx dashboard](https://github.com/37chairs/picotron-wx-dashboard/blob/master/doc/wx_data_output.png?raw=true)
+![Picotron wx dashboard](https://github.com/37chairs/picotron-wx-dashboard/blob/master/doc/dashboard_preview.png?raw=true)
 
 ## host_wx_service.rb
 
-Runs on host computer and fetches wx data from an API, then writes to a shared location where Picotron can read it.  
-We could do this as POD (Picotron Object Data) compressed as lz4, but I prefer simple hex strings.  Maybe if other people
-touch this someday, we'll switch to POD or maybe even our own TLV format, though I don't really want to write a TLV 
-parser in lua. 
-
-Format: (Quickly changing)
-- byte 1: hour
-- byte 2: minute
-- byte 3: seconds
-- byte 4: temperature
-- byte 5: humidity
-- byte 6 - 7: pressure
-- byte 8: wind speed
+Runs on host computer and fetches wx data from an API, then writes to a shared location where Picotron can read it 
+as a lua table.
 
 ## main.lua
 
--- main.lua
+++ main.lua
@@ -1,39 +1,54 @@
---[[pod_format="raw",created="2024-05-19 03:47:03",modified="2024-05-19 11:51:50",revision=31]]
-wx_data = fetch("/appdata/wx.dat")
-print("Raw wx data: " .. wx_data)
+--[[pod_format="raw",created="2024-05-19 03:47:03",modified="2024-05-19 11:51:50",revision=31]] update_timer = 0
 
-function get_bytes(hex_string, start_byte, end_byte)
-  -- Remove the '0x' prefix
-  hex_string = hex_string:sub(3)
+wx_data = {}
 
-  -- Calculate the start and end positions in the string
-  local start_pos = (start_byte - 1) * 2 + 1
-  local end_pos = end_byte * 2
-
-  -- Extract the substring
-  local byte_string = hex_string:sub(start_pos, end_pos)
-
-  print("Byte string: " .. byte_string)
-
-  -- Convert the hex substring to an integer
-  local integer_value = tonumber(byte_string, 16)
-  return integer_value
+function _init()
 end
 
-function hex_to_integer(bytes)
-  -- bytes is a string of hex bytes
-  -- returns an integer
+function _update()
+    update_timer = update_timer - 1
 
-  return tonumber(bytes, 16)
+    if update_timer < 1 then
+        update_timer = 15
+        wx_data = fetch("/appdata/wx.pod")
+    end
 end
 
-print(" ")
-print("Time: " .. get_bytes(wx_data, 1, 1) .. ":" ..get_bytes(wx_data, 2, 2) .. ":" .. get_bytes(wx_data, 3, 3))
-print(" ")
-print("Temperature: " .. get_bytes(wx_data, 4, 4) .. " F")
-print(" ")
-print("Humidity: " .. get_bytes(wx_data, 5, 5) .. " %")
-print(" ")
-print("Pressure: " .. get_bytes(wx_data, 6, 7) .. " mb")
-print(" ")
-print("Wind Speed: " .. get_bytes(wx_data, 8, 8) .. " mph")
+function _draw()
+    cls()
+
+    print("Currently: ", 5, 10)
+
+    print(wx_data.currently.summary, 5, 30)
+    print("Temp: " .. wx_data.currently.temperature .. "F (Feels like " .. wx_data.currently.feels_like .. "F)", 5, 40)
+    print("Humidity: " .. wx_data.currently.humidity * 100 .. "%", 5, 50)
+    print("Wind: " .. wx_data.currently.wind_speed .. " mph bearing " .. wx_data.currently.wind_bearing ..
+              " (Gusting to " .. wx_data.currently.wind_gust .. " mph)", 5, 60)
+    print("Cloud cover: " .. wx_data.currently.cloud_cover * 100 .. "%", 5, 70)
+    print("Pressure: " .. wx_data.currently.pressure .. " mb", 5, 80)
+    print("Precip probability: " .. wx_data.currently.precip_probability * 100 .. "%", 5, 90)
+
+    line(0, 107, 480, 107, 3)
+
+    for i = 1, 5 do
+        local x = 5 + ((i-1) * 90)
+        print(wx_data.daily[i].day_of_week .. ": " , x, 120, 7)
+        print(wx_data.daily[i].summary, x, 140)
+        print("High: " .. wx_data.daily[i].temperature_high .. "F", x, 150)
+        print("Low: " .. wx_data.daily[i].temperature_low .. "F", x, 160)
+        print("Precip: " .. wx_data.daily[i].precip_probability * 100 .. "%", x, 170)
+        print("Wind: " .. wx_data.daily[i].wind_speed .. " mph", x, 180)
+        print("Clouds: " .. wx_data.daily[i].cloud_cover * 100 .. "%", x, 190)
+        print("Sunrise: " .. string.format("%02d", wx_data.daily[i].sunrise_hour) .. ":" .. string.format("%02d", wx_data.daily[i].sunrise_minute), x, 200)
+    end
+
+    line(0, 240, 480, 240, 3)
+
+    print("Updated: " .. string.format("%02d", wx_data.time_hour) .. ":" .. string.format("%02d", wx_data.time_minute) .. ":" .. string.format("%02d", wx_data.time_second), 5, 250, 7)
+    print("Elevation: " .. wx_data.elevation, 95, 250)
+    print("Lat / Lon: " .. wx_data.latitude .. "," .. wx_data.longitude, 5, 260)
+
+    spr(1, 468, 254)
+    print("37chairs.com", 405, 260)
+end
+
